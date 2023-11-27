4aa4deecca4aa01762e905f68da5640227fe22fc
prem7486 <luckypremchd13@gmail.com>
2023-11-27 18:19:53 +0530

    Calculator
    Basic Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,34 @@
+let input = document.getElementById('inputBox');
+let btns = document.querySelectorAll('button');
+
+let string = "";
+
+let arr = Array.from(btns);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let delButton = document.getElementById("del")
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target === delButton || e.target.parentNode === delButton){
+            if (typeof string === 'string') {
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            } else {
+                string = string.toString().slice(0, -1);
+                input.value = string;
+            }
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        ;
+    })
+})
